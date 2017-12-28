1d27667e4daba12d4cd65e68c2910d246e5ac198
yiyan <yiyer@users.noreply.github.com>
2017-12-28 10:18:09 +0800

    init

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    WebCamDevice webCamDevice;
+    WebCamTexture webCamTexture;
+
+    int width = 800;
+    int height = 600;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        webCamDevice = WebCamTexture.devices[0];
+        webCamTexture = new WebCamTexture(webCamDevice.name, width, height);
+        webCamTexture.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
