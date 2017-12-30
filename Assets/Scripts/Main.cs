e302eddba5970739576660f6aec1e0b73b8e671f
yiyan <yiyer@users.noreply.github.com>
2017-12-30 13:15:58 +0800

    add

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,8 @@ public class Main : MonoBehaviour
 
     WebCamDevice webCamDevice;
     WebCamTexture webCamTexture;
+    Renderer renderer;
+    bool isPlay = false;
 
     int width = 800;
     int height = 600;
@@ -15,10 +17,13 @@ public class Main : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        renderer = gameObject.GetComponent<Renderer>();
+
         webCamDevice = WebCamTexture.devices[0];
         webCamTexture = new WebCamTexture(webCamDevice.name, width, height);
-        webCamTexture.Play();
 
+        webCamTexture.Play();
+        renderer.material.mainTexture = webCamTexture;
     }
 
     // Update is called once per frame
